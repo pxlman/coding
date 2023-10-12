a8d5b130d0e484b9aea4d0571651f6dfc75ffcbe
pxlman <agxgv7573@gmail.com>
2023-10-12 21:33:21 +0300

    Missing around

--- bash/README1.md
+++ bash/README1.md
@@ -2,10 +2,10 @@
 
 ### instruction
 
-'''bash
+```bash
 git clone https://github.com/pxlman/coding
 cd coding
 cd bash
 chmod +x if_response.sh
 ./if_response.sh
-'''
+```
--- bash/if_response.sh
+++ bash/if_response.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/bash
 
+# Waiting a link to response, and 
+
 while true
 do
+  SECONDS=0
   if wget --server-response --spider $1
   then
-    echo done
-    paplay $BEEP
-    exit 0
+    if [ $SECONDS -lt 5 ]; then
+      echo done
+      paplay $BEEP
+      exit 0
+    fi
   else
     echo false
-    paplay $BEEP
   fi
   sleep 1
 done
